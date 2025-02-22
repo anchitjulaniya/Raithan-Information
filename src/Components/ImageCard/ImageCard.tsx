8b3892d347da732d26613a78f708f22f8504bf6a
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2025-02-22 11:45:29 +0530

    adding scale css in Image Card

--- src/Components/ImageCard/ImageCard.tsx
+++ src/Components/ImageCard/ImageCard.tsx
@@ -9,11 +9,11 @@ interface ImageCardProps {
 
 const ImageCard: React.FC<ImageCardProps> = ({Click, imageSrc, altText, label }) => {
   return (
-    <div onClick={Click} className="mt-5 mb-5 flex flex-col py-3 px-6 items-center justify-between h-fit md:h-[360px] md:min-w-[280px] w-fit gap-7 shadow-lg hover:cursor-pointer rounded-xl">
+    <div onClick={Click} className="group mt-5 mb-5 flex flex-col py-3 px-6 items-center justify-between h-fit md:h-[360px] md:min-w-[280px] w-fit gap-7 shadow-lg hover:cursor-pointer rounded-xl overflow-hidden">
       <span>
-        <img src={imageSrc} className={`max-w-[270px] min-w-[270px]  md:max-w-[180px] w-fit`} alt={altText} />
+        <img src={imageSrc} className={`group-hover:scale-125 duration-300 group-hover:rounded-2xl max-w-[270px] min-w-[270px]  md:max-w-[180px] w-fit`} alt={altText} />
       </span>
-      <span className="text-black w-fit">{label}</span>
+      <span className="text-black w-fit group-hover:scale-125 group-hover:font-semibold duration-300">{label}</span>
     </div>
   );
 };
