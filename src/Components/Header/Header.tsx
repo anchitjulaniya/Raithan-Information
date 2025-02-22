dcc82303db2969ccb4d139cb959e078903a780f1
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2025-02-22 11:50:06 +0530

    removing margin in mobile view below header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,9 +3,9 @@ import Logo from '../../assets/Images/raitanlogo.png'
 
 const Header: React.FC = () => {
   return (
-    <div className='flex justify-between px-5 py-2 w-full mb-4 shadow-lg'>
+    <div className='flex justify-between px-5 py-2 w-full md:mb-4 shadow-lg'>
         <img src={Logo} className='w-[60px]' alt="Logo" />
-        <div className='h- flex items-center'>More</div>
+        <div className='h-full flex items-center'>More</div>
     </div>
   )
 }
